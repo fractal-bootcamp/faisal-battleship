09e9ad412c213f343712a3a5745bcc0fcb17167e
Faisal <faisal@owimer.co>
2024-10-02 14:58:00 -0400

    start page component logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useState } from "react"
+import StartPage from "./components/StartPage"
 
+type GameMode = "1vs1" | "1vsComputer"
 
-const App = () => {
+const App: React.FC = () => {
+  const [gameStarted, setGameStarted] = useState<boolean>(false)
+  const [player1Name, setPlayer1Name] = useState<string>("")
+  const [player2Name, setPlayer2Name] = useState<string | null>(null)
+  const [mode, setmode] = useState<GameMode>("1vs1")
 
+  // Handle startgame logic
+  const startGame = (
+    player1: string,
+    player2: string | null,
+    selectedMode: GameMode,
+  ) => {
+    setPlayer1Name(player1)
+    setPlayer2Name(player2)
+    setmode(selectedMode)
+    setGameStarted(true)
+  }
 
   return (
     <div>
-      I am alive
+      <StartPage
+        OnGameStart={startGame}
+      />
     </div>
   )
 }
