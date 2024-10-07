dcc526b35b168944392dde4b001c26f936f5553e
Faisal <faisal@owimer.co>
2024-10-07 08:45:19 -0400

    refactored game code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
+import Game from "./Game";
+import "./index.css"
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <Router>
-    <App />
+    <Routes>
+      <Route path="/game/:id" element={<App />} />
+      <Route path="/" element={<Game />} />
+
+    </Routes>
+
   </Router>
 );
